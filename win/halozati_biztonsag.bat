4afa8e3c6c3488cd63d281f981910a8a57ae732a
LordAthis <lordathis@gmail.com>
2026-08-24 14:09:19 +0200

    Refactor network security report script output

--- win/halozati_biztonsag.bat
+++ win/halozati_biztonsag.bat
@@ -1,43 +1,61 @@
 @echo off
 color 0c
 title Halozati Biztonsag Ellenorzo
+
+REM Script konyvtara es LOG mappa
+set "SCRIPT_DIR=%~dp0"
+set "LOG_DIR=%SCRIPT_DIR%LOG"
+
+REM LOG mappa letrehozasa, ha nem letezik
+if not exist "%LOG_DIR%" mkdir "%LOG_DIR%"
+
 set "datum=%date:~0,10%"
 set "ido=%time:~0,8%"
 set "biztonsagi_jelentes=biztonsagi_jelentes_%datum%_%ido%.txt"
 set "biztonsagi_jelentes=%biztonsagi_jelentes::=-%"
 set "biztonsagi_jelentes=%biztonsagi_jelentes: =_%"
 set "biztonsagi_jelentes=%biztonsagi_jelentes:/=-%"
-echo ================================================ > %biztonsagi_jelentes%
+
+REM Teljes utvonal a LOG mappaba
+set "kimenet=%LOG_DIR%\%biztonsagi_jelentes%"
+
+echo ================================================ > "%kimenet%"
 echo        HALOZATI BIZTONSAGI JELENTES
 echo        %datum% %ido%
-echo ================================================ >> %biztonsagi_jelentes%
-echo. >> %biztonsagi_jelentes%
+echo ================================================ >> "%kimenet%"
+echo. >> "%kimenet%"
+
 echo 1. Nyitott portok ellenorzese...
-echo 1. NYITOTT PORTOK: >> %biztonsagi_jelentes%
-echo ------------------ >> %biztonsagi_jelentes%
-netstat -an | findstr LISTENING >> %biztonsagi_jelentes%
-echo. >> %biztonsagi_jelentes%
-echo 2. Kimenő kapcsolatok...
-echo 2. KIMENO KAPCSOLATOK: >> %biztonsagi_jelentes%
-echo ---------------------- >> %biztonsagi_jelentes%
-netstat -an | findstr ESTABLISHED >> %biztonsagi_jelentes%
-echo. >> %biztonsagi_jelentes%
+echo 1. NYITOTT PORTOK: >> "%kimenet%"
+echo ------------------ >> "%kimenet%"
+netstat -an | findstr LISTENING >> "%kimenet%"
+echo. >> "%kimenet%"
+
+echo 2. Kimeno kapcsolatok...
+echo 2. KIMENO KAPCSOLATOK: >> "%kimenet%"
+echo ---------------------- >> "%kimenet%"
+netstat -an | findstr ESTABLISHED >> "%kimenet%"
+echo. >> "%kimenet%"
+
 echo 3. Processzek halozati aktivitasa...
-echo 3. PROCESSZEK HALOZATI AKTIVITASA: >> %biztonsagi_jelentes%
-echo ---------------------------------- >> %biztonsagi_jelentes%
-netstat -ano | findstr /v "0.0.0.0" >> %biztonsagi_jelentes%
-echo. >> %biztonsagi_jelentes%
+echo 3. PROCESSZEK HALOZATI AKTIVITASA: >> "%kimenet%"
+echo ---------------------------------- >> "%kimenet%"
+netstat -ano | findstr /v "0.0.0.0" >> "%kimenet%"
+echo. >> "%kimenet%"
+
 echo 4. Tuzfal allapot...
-echo 4. TUZFAL ALLAPOT: >> %biztonsagi_jelentes%
-echo ------------------ >> %biztonsagi_jelentes%
-netsh advfirewall show allprofiles state >> %biztonsagi_jelentes%
-echo. >> %biztonsagi_jelentes%
+echo 4. TUZFAL ALLAPOT: >> "%kimenet%"
+echo ------------------ >> "%kimenet%"
+netsh advfirewall show allprofiles state >> "%kimenet%"
+echo. >> "%kimenet%"
+
 echo 5. Kulso IP cim...
-echo 5. KULSO IP CIM: >> %biztonsagi_jelentes%
-echo ---------------- >> %biztonsagi_jelentes%
-nslookup myip.opendns.com resolver1.opendns.com >> %biztonsagi_jelentes%
+echo 5. KULSO IP CIM: >> "%kimenet%"
+echo ---------------- >> "%kimenet%"
+nslookup myip.opendns.com resolver1.opendns.com >> "%kimenet%"
+
 echo.
 echo Biztonsagi ellenorzes kesz!
-echo Jelentes: %biztonsagi_jelentes%
+echo Jelentes: %kimenet%
 echo.
 pause
